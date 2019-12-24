2f53e9c72d69cd45958c2bcc60a8cd3a451b69e4
Petr Sahula <spetex@icloud.com>
2019-12-24 15:39:00 +0100

    Enable extended markdown features

--- src/utils.rb
+++ src/utils.rb
@@ -2,7 +2,23 @@ require 'liquid'
 require 'redcarpet'
 
 class Hash; alias + merge end
-MARKDOWN = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+MARKDOWN_EXTENSIONS = {
+  autolink: true,
+  disable_indented_code_blocks: true,
+  fenced_code_blocks: true,
+  footnotes: true,
+  highlight: true,
+  lax_spacing: true,
+  no_intra_emphasis: true,
+  quote: true,
+  space_after_headers: true,
+  strikethrough: true,
+  superscript: true,
+  tables: true,
+  underline: true
+}.freeze
+
+MARKDOWN = Redcarpet::Markdown.new(Redcarpet::Render::HTML, MARKDOWN_EXTENSIONS)
 
 def parse_template(template)
   Liquid::Template.parse(template)
