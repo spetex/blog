838b88f9745bff0ed805459524378612e5dbd7a7
Petr Sahula <spetex@icloud.com>
2019-12-31 20:22:30 +0100

    Fix footer spacing

--- layouts/assets/src/Footer.svelte
+++ layouts/assets/src/Footer.svelte
@@ -11,19 +11,16 @@
     align-items: center;
     padding-bottom: 2rem;
   }
-  footer p {
-    padding-left: 0;
-    padding-right: 0;
-  }
   section {
     width: 100%;
-    max-width: 800px;
+    max-width: 855px;
     flex-grow: 1;
+    margin-top: 1.5rem;
   }
 </style>
 
 <footer>
   <section>
-    <p>Some profound footer content here.</p>
+    <div>Some profound footer content here.</div>
   </section>
 </footer>
