9787d009aa8eb829578b086911a7db4ca00e5289
Petr Sahula <spetex@icloud.com>
2020-01-11 14:15:19 +0100

    Footer update

--- layouts/assets/src/Footer.svelte
+++ layouts/assets/src/Footer.svelte
@@ -4,12 +4,21 @@
 <style>
   footer {
     width: 100%;
-    background: var(--color-secondary-bg);
-    color: var(--color-primary-bg);
+    background: var(--color-base-secondary-bg);
+    color: var(--color-base-secondary-fg);
     display: flex;
     flex-flow: column;
     align-items: center;
     padding-bottom: 2rem;
+  }
+  a {
+    color: var(--color-accent-highlight);
+  }
+  a:active {
+    color: var(--color-accent-active);
+  }
+  ul {
+    list-style: none;
   }
   section {
     width: 100%;
@@ -17,11 +26,21 @@
     flex-grow: 1;
     margin-top: 1.5rem;
   }
+  nav {
+    display: flex;
+  }
+  h1 {
+    font-weight: 400;
+  }
+  h1 > a {
+    color: var(--color-base-primary-bg);
+    text-decoration: none;
+  }
 </style>
 
 <footer>
   <section>
-    <h1>spetex minis</h1>
+    <h1><a href="https://spetex-minis.com">spetex minis</a></h1>
     <p>
       Writing about my journey to become an artisan miniature painter.
     </p>
@@ -32,8 +51,6 @@
         <li><a href="/gallery">Gallery</a></li>
         <li><a href="/about">About</a></li>
       </ul>
-    </nav>
-    <nav>
       <ul>
         <li><a href="https://www.instagram.com/spetex.miniatures/">Instagram</a></li>
         <li><a href="https://paintpad.app/spetex">PaintPad</a></li>
