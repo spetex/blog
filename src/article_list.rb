787deaf6d08743b6a9df6fef10f5f4f4c610654b
Petr Sahula <spetex@icloud.com>
2020-01-11 19:51:03 +0100

    Sort articles by date

--- src/article_list.rb
+++ src/article_list.rb
@@ -1,14 +1,19 @@
 def read_articles
-  Dir.children('./articles')
-     .map do |filename|
-       config, _article_markdown = read_article filename.split('.')[0]
-       read_vars(config) + { 'slug' => filename.split('.')[0] }
-     end
+  Dir.children('./articles').map do |filename|
+    config, _article_markdown = read_article filename.split('.')[0]
+    read_vars(config) + { 'slug' => filename.split('.')[0] }
+  end
+end
+
+def format_articles(articles)
+  articles.map { |article| article + { 'published' => Date.parse(article['published']) } }
+          .sort_by { |article| article['published'] }
+          .reverse
 end
 
 def render_article_list
   body = get_template('layouts/article_list.html')
-         .render 'articles' => read_articles
+         .render 'articles' => format_articles(read_articles)
   vars = {
     'title' => 'Articles',
     'svelte' => true
