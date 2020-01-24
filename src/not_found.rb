d98a8c70af479dc5ae632ff460467744cf60c08d
Petr Sahula <spetex@icloud.com>
2020-01-24 16:32:13 +0100

    Add basic 404 that actually says something

--- src/not_found.rb
+++ src/not_found.rb
@@ -3,7 +3,8 @@ def render_not_found(response)
   body = get_template('layouts/404.html')
          .render
   vars = {
-    'title' => 'Page not found!'
+    'title' => 'Page not found!',
+    'svelte' => true
   }
   final_render(body, vars)
 end
